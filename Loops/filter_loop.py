d4d9655ab55cd47ea1cf56b06a37bccebacac1df
phillib <phillib@users.noreply.github.com>
2015-02-28 18:07:13 -0600

    Update and rename Loops to Loops/filter_loop.py

---
+++ Loops/filter_loop.py
@@ -0,0 +1,7 @@
+print 'Before'
+for value in [9, 41, 12, 3, 74, 15]:
+    if value > 20:
+        print 'Large number', value
+print 'After'
+
+# This code will use a loop to search for any number greater than 20 in the list and print it.
