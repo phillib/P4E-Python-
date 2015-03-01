6b91113feefa38e4eb030220d42593411ef59cdd
phillib <phillib@users.noreply.github.com>
2015-02-28 18:21:39 -0600

    Update Raw_Input_Loop.py

--- Loops/Raw_Input_Loop.py
+++ Loops/Raw_Input_Loop.py
@@ -4,3 +4,7 @@ while True:
         break
     print line
 print "Done!"
+
+# This while loop will continue to prompt the user to enter a string
+# until the user enters the word "done".  
+# Once "done" is entered, the loop is exited and the string "Done!" is printed.
